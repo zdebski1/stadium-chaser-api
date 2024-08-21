e8e503666b8f7475cb05588256afe0cb25c574cf
zdebs <zdebski1@gmail.com>
2024-08-20 22:17:34 -0400

    get endpoint to return list of stadiums

---
+++ Controllers/StadiumsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using stadiumChaserApi.Repositories;
+using stadiumChaserApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace stadiumChaserApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StadiumsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public StadiumsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Stadium>>> GetStadiums()
+        {
+            var stadiums = await _context.Stadiums.ToListAsync();
+            return Ok(stadiums);
+        }
+    }
+}
