83973d806366953d81d976e9743554d0a7565d38
zdebs <zdebski1@gmail.com>
2024-08-20 21:47:32 -0400

    updating folder structure

---
+++ Repositories/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using stadiumChaserApi.Entities;
+
+namespace stadiumChaserApi.Repositories
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Record> Records { get; set; }
+    }
+}
